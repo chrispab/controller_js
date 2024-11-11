5b4b9911b433435c28e0637408c7311d989c3fb5
chrispab <cbattisson@gmail.com>
2024-11-11 22:26:31 +0000

    re

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
 // client/src/App.js
 
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+    // fetch("/api")
+    //   .then((res) => res.json())
+    //   .then((data) => setData(data.message));
+
+    async function fetchUsers() {
+      const fullResponse = await fetch('/api');
+      const responseJson = await fullResponse.json();
+      setData(responseJson.message);
+    }
+    setInterval(() => {
+      fetchUsers();
+    }, 5000);
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <p>{!data ? 'Loading...' : data}</p>
       </header>
     </div>
   );
