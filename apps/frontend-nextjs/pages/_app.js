3cf1b022a50666dc5ad3e05626819cb9c0c2065a
chrispab <cbattisson@gmail.com>
2025-07-24 16:47:48 +0100

    favicon

--- apps/frontend-nextjs/pages/_app.js
+++ apps/frontend-nextjs/pages/_app.js
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
