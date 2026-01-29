593e371442432cee0345c4f12c354cc707af3051
chrispab <cbattisson@gmail.com>
2026-01-29 20:22:39 +0000

    zoneinfo

---
+++ apps/controller/src/config/zoneInfo_example.js
@@ -0,0 +1,10 @@
+// Example zone information configuration file
+// modify this file to set zone-specific parameters
+// and save it as zoneInfo.js in the same directory
+const zoneInfo = {
+    "name": "zone3",
+    "id": 3,
+    "configFileName": "zone3_config.json"
+}
+
+export default zoneInfo;
