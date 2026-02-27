93ba9f509004d4429567a122430336b07876b5af
chrispab <cbattisson@gmail.com>
2026-02-27 22:34:43 +0000

    vent ctl

--- apps/controller/src/components/vent.js
+++ apps/controller/src/components/vent.js
@@ -5,7 +5,7 @@ import cfg from '../services/config.js';
 import * as utils from '../utils/utils.js';
 import eventEmitter from '../services/eventEmitter.js';
 
-const logLevel = 'debug';
+const logLevel = 'error'; // Change to 'debug' for more verbose logging
 
 export default class Vent {
   constructor(name, ventPowerPin, ventSpeedPin) {
@@ -33,7 +33,7 @@ export default class Vent {
     eventEmitter.on('temperatureChanged', ({ temperature }) => {
       this.currentTemp = temperature;
     });
-    
+
     eventEmitter.on('outsideTemperatureChanged', ({ outsideTemperature }) => {
       this.outSideTemp = outsideTemperature;
     });
@@ -49,6 +49,108 @@ export default class Vent {
     this.periodicPublication();
     setInterval(() => this.periodicPublication(), cfg.get('vent.periodicPublishIntervalMs'));
   }
+
+  controlCycle() {
+    const setPoint = this.lightState ? cfg.get('zone.highSetpoint') : cfg.get('zone.lowSetpoint');
+    const elapsedMs = Date.now() - this.lastStateChangeMs;
+
+    logger.log(
+      logLevel,
+      `====>>Control Cycle Start - Current Temp: ${this.currentTemp}°C, Setpoint: ${setPoint}°C, Light State: ${this.lightState ? 'ON' : 'OFF'}, Vent Override: ${this.ventOverride}`,
+    );
+    // --- High-Temperature Override Check ---
+    // when day and temp is above highsp activate override and run at 100% if its light
+    if (this.lightState && this.currentTemp > setPoint) {
+      logger.log(logLevel, `====Current Temp: ${this.currentTemp}°C exceeds high setpoint (${setPoint}°C) during light conditions`);
+      if (!this.ventOverride) {
+        logger.log(logLevel, '====High-temperature override ACTIVATED');
+        this.ventOverride = true;
+      }
+      this.updateState(2); // Set to 100% speed
+      return; // Override takes precedence
+    }
+    // when dark and temp is above lowsp + deadband activate override and run at 50% to avoid bringing in too much cold air
+    if (!this.lightState && this.currentTemp > setPoint + cfg.get('vent.setpointDeadBandSize')) {
+      logger.log(logLevel, `====Current Temp: ${this.currentTemp}°C exceeds low setpoint + deadband (${setPoint + cfg.get('vent.setpointDeadBandSize')}°C) during dark conditions`);
+      if (!this.ventOverride) {
+        logger.log(logLevel, '====High-temperature override ACTIVATED');
+        this.ventOverride = true;
+      }
+      this.updateState(1); // Set to 50% speed
+      return; // Override takes precedence
+    }
+
+    // // if (this.currentTemp > setPoint + cfg.get('vent.setpointDeadBandSize')) {
+    // if (this.currentTemp > setPoint) {
+    //   logger.log(logLevel, `====Current Temp: ${this.currentTemp}°C exceeds setpoint + deadband (${setPoint + cfg.get('vent.setpointDeadBandSize')}°C)`);
+    //   if (!this.ventOverride) {
+    //     logger.log(logLevel, '====High-temperature override ACTIVATED');
+    //     this.ventOverride = true;
+    //   }
+    //   // if its light run at 100% otherwise if its dark and the lowsp diff is lt 5 run at 100% otherwise run at 50% to avoid bringing in too much cold air
+    //   if (this.lightState) {
+    //     this.updateState(2); // Set to 100% speed
+    //   } else {
+    //     logger.log(
+    //       logLevel,
+    //       `====Low setpoint: ${cfg.get('zone.lowSetpoint')}, Outside Temp: ${this.outSideTemp}, Diff: ${cfg.get('zone.lowSetpoint') - this.outSideTemp}, Threshold: ${cfg.get('vent.outsideTempDiffThresholdForMaxSpeed')}`,
+    //     );
+    //     // if outside temperature and lowsp diff is lt 5 run at 100% otherwise run at 50% to avoid bringing in too much cold air
+    //     // if (cfg.get('zone.lowSetpoint') - this.outSideTemp < cfg.get('vent.outsideTempDiffThreshold')) {
+    //     if (cfg.get('zone.lowSetpoint') - this.outSideTemp < cfg.get('vent.outsideTempDiffThresholdForMaxSpeed')) {
+    //       this.updateState(2); // Set to 100% speed
+    //     } else {
+    //       this.updateState(1); // Set to 50% speed
+    //     }
+    //   }
+    //   return; // Override takes precedence
+    // }
+
+    // --- Deactivate Override ---
+    if (this.ventOverride) {
+      if (this.currentTemp <= setPoint) {
+        if (elapsedMs >= cfg.get('vent.ventOverridePulseOnDelta')) {
+          logger.log(logLevel, '====High-temperature override DEACTIVATED');
+          this.ventOverride = false;
+          this.cycleState = 'inactive'; // Reset cycle
+          this.updateState(0); // Turn off
+        }
+      }
+      return; // Wait for override to complete
+    }
+
+    // --- Regular Day/Night Cycle Logic ---
+    const dayOrNight = this.lightState ? 'day' : 'night';
+    this.setOnDurationMs(cfg.get(`vent.onDurationMs.${dayOrNight}`));
+    this.setOffDurationMs(cfg.get(`vent.offDurationMs.${dayOrNight}`));
+
+    logger.log(
+      logLevel,
+      `====Cycle State: ${this.cycleState}, Elapsed Time: ${elapsedMs}ms, On Duration: ${this.getOnDurationMs()}ms, Off Duration: ${this.getOffDurationMs()}ms`,
+    );
+    switch (this.cycleState) {
+      case 'inactive':
+        // Start a new cycle
+        this.cycleState = 'ON';
+        this.updateState(1); // 50% speed
+        break;
+
+      case 'ON':
+        if (elapsedMs >= this.getOnDurationMs()) {
+          this.cycleState = 'OFF';
+          this.updateState(0);
+        }
+        break;
+
+      case 'OFF':
+        if (elapsedMs >= this.getOffDurationMs()) {
+          this.cycleState = 'ON';
+          this.updateState(1); // 50% speed
+        }
+        break;
+    }
+  }
+
   updateState(newState) {
     const oldState = this.getState();
     if (newState !== oldState) {
@@ -107,72 +209,6 @@ export default class Vent {
 
   getOffDurationMs() {
     return this.offDurationMs;
-  }
-
-  controlCycle() {
-    const setPoint = this.lightState ? cfg.get('zone.highSetpoint') : cfg.get('zone.lowSetpoint');
-    const elapsedMs = Date.now() - this.lastStateChangeMs;
-
-    // --- High-Temperature Override Check ---
-    if (this.currentTemp > setPoint + cfg.get('vent.setpointDeadBandSize')) {
-      if (!this.ventOverride) {
-        logger.debug('High-temperature override ACTIVATED');
-        this.ventOverride = true;
-      }
-      // if its light run at 100% otherwise if its dark and the lowsp diff is lt 5 run at 100% otherwise run at 50% to avoid bringing in too much cold air
-      if (this.lightState) {
-        this.updateState(2); // Set to 100% speed
-      } else {
-        // if outside temperature and lowsp diff is lt 5 run at 100% otherwise run at 50% to avoid bringing in too much cold air
-        // if (cfg.get('zone.lowSetpoint') - this.outSideTemp < cfg.get('vent.outsideTempDiffThreshold')) {
-        if (cfg.get('zone.lowSetpoint') - this.outSideTemp < cfg.get('vent.outsideTempDiffThresholdForMaxSpeed')) {
-          this.updateState(2); // Set to 100% speed
-        } else {
-          this.updateState(1); // Set to 50% speed
-        }
-      }
-      return; // Override takes precedence
-    }
-
-    // --- Deactivate Override ---
-    if (this.ventOverride) {
-      if (this.currentTemp <= setPoint) {
-        if (elapsedMs >= cfg.get('vent.ventOverridePulseOnDelta')) {
-          logger.debug('High-temperature override DEACTIVATED');
-          this.ventOverride = false;
-          this.cycleState = 'inactive'; // Reset cycle
-          this.updateState(0); // Turn off
-        }
-      }
-      return; // Wait for override to complete
-    }
-
-    // --- Regular Day/Night Cycle Logic ---
-    const dayOrNight = this.lightState ? 'day' : 'night';
-    this.setOnDurationMs(cfg.get(`vent.onDurationMs.${dayOrNight}`));
-    this.setOffDurationMs(cfg.get(`vent.offDurationMs.${dayOrNight}`));
-
-    switch (this.cycleState) {
-      case 'inactive':
-        // Start a new cycle
-        this.cycleState = 'ON';
-        this.updateState(1); // 50% speed
-        break;
-
-      case 'ON':
-        if (elapsedMs >= this.getOnDurationMs()) {
-          this.cycleState = 'OFF';
-          this.updateState(0);
-        }
-        break;
-
-      case 'OFF':
-        if (elapsedMs >= this.getOffDurationMs()) {
-          this.cycleState = 'ON';
-          this.updateState(1); // 50% speed
-        }
-        break;
-    }
   }
 
   periodicPublication() {
